5f8271064774bc343b4d30d8928dd4a7ee5e288e
James Tripp <james.tripp@warwick.ac.uk>
2023-07-02 14:46:02 +0000

    Trial sphinx build

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -9,7 +9,7 @@
 project = "Greenwashing"
 copyright = "2023, James Tripp"
 author = "James Tripp"
-release = "0.0.0"
+release = "0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
